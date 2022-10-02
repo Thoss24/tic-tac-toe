8e5578e1681dcd83352e6cf3db96cbe7a4fca5a0
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-02 15:56:10 +0100

    title added/ hover event added to board squares

--- script.js
+++ script.js
@@ -14,14 +14,26 @@ const Player = (player) => {
 
 
 function createBoard() {
+
     let board = document.querySelector('#board-container');
+    let boardSquare = document.createElement('div');
+
     for (let i = 0; i < 9; i++) {
         let boardSquare = document.createElement('div');
         boardSquare.style.border = '1px solid black'
         board.style.gridTemplateColumns = `repeat(3, 1fr)`;
         board.style.gridTemplateRows = `repeat(3, 1fr)`;
+
+        boardSquare.addEventListener('mouseenter', () => {
+            boardSquare.style.background = 'lightgrey'
+        });
+        boardSquare.addEventListener('mouseleave', () => {
+            boardSquare.style.background = 'white';
+        })
+
         board.appendChild(boardSquare)
-    }
-    
+    };
 }
 createBoard()
+
+
