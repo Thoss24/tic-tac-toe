aa2e85cc9802c012023d46bab9f016f63a5fa0d3
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-03 16:02:13 +0100

    function to append playerchoice unfinished

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+
+let playerChoice = {
+    getChoice() {
+        return this.choice;
+    },
+};
+
+// Players, factory-function
+function players(choice) {
+   let player = Object.create(playerChoice);
+   player.choice = choice;
+   return player
+};
+
+let tokenChoice = (function() {
+
+    let buttonSelection = {
+    buttonX: document.querySelector('#choice-x'),
+    buttonO: document.querySelector('#choice-o'),
+    }
+    console.log(buttonSelection)
+
+    let playerChoice = {
+        playerX: players('X'),
+        playerO: players('O'),
+    }
+    console.log(playerChoice)
+
+    buttonSelection.buttonX.addEventListener('click', (e) => {
+        console.log(e.target);
+    });
+
+    buttonSelection.buttonO.addEventListener('click', (e) => {
+        console.log(e.target);
+    });
+
+    return {
+        buttonSelection: buttonSelection,
+        playerChoice: playerChoice,
+    }
+
+})();
+
+
+
 // Game-board module
 let gameBoard = (function() {
     'use strict'
@@ -6,47 +51,29 @@ let gameBoard = (function() {
         board: [],
     };
 
-    let playerChoice = {
-        getChoice() {
-            return this.choice;
-        },
-    };
-    
-    // Players, factory-function
-    function players(choice) {
-       let player = Object.create(playerChoice);
-       player.choice = choice;
-       return player
-    };
-
-    let playerX = players('X');
-    let playerO = players('O');
-
     // create tic-tac-toe board & append "X" or "O" on click event
     function createBoard() {
 
-        let board = document.querySelector('#board-container');
+    let board = document.querySelector('#board-container');
     
-        for (let i = 0; i < 9; i++) {
-            let boardSquare = document.createElement('div');
-            boardSquare.style.border = '1px solid black'
-            board.style.gridTemplateColumns = `repeat(3, 1fr)`;
-            board.style.gridTemplateRows = `repeat(3, 1fr)`;
+    for (let i = 0; i < 9; i++) {
+        let boardSquare = document.createElement('div');
+        boardSquare.style.border = '1px solid black'
+        board.style.gridTemplateColumns = `repeat(3, 1fr)`;
+        board.style.gridTemplateRows = `repeat(3, 1fr)`;
 
-            boardSquare.addEventListener('click', () => {
-                boardSquare.textContent = playerX.getChoice();
-            })
+        boardSquare.addEventListener('click', () => {
+            boardSquare.textContent = players('X').getChoice()
+        })
         
-            board.appendChild(boardSquare)
-        };
-    }
-    createBoard();
-
-
-    
-    return {
-        ticTacToeBoard: ticTacToeBoard
+        board.appendChild(boardSquare)
     };
+}
+    createBoard();
+    
+        return {
+            ticTacToeBoard: ticTacToeBoard
+        };
 
 })();
 
