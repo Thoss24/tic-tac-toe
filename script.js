714fec1befb4672359fc55088e13f1b1b609b073
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-03 12:25:44 +0100

    onclick event appends X to board and pushes value to gameboard array

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
+// Game-board module
 let gameBoard = (function() {
     'use strict'
 
     let ticTacToeBoard = {
         board: [],
+    };
+
+    // create tic-tac-toe board & append "X" or "O" on click event
+    function createBoard() {
+
+        let board = document.querySelector('#board-container');
+    
+        for (let i = 0; i < 9; i++) {
+            let boardSquare = document.createElement('div');
+            boardSquare.style.border = '1px solid black'
+            board.style.gridTemplateColumns = `repeat(3, 1fr)`;
+            board.style.gridTemplateRows = `repeat(3, 1fr)`;
+    
+            boardSquare.addEventListener('click', () => {
+                boardSquare.textContent = 'X';
+                ticTacToeBoard.board.push(boardSquare.textContent)
+            });
+    
+            board.appendChild(boardSquare)
+        };
     }
+    createBoard()
+    
+    return {
+        ticTacToeBoard: ticTacToeBoard
+    };
 
 })();
 
 
+// Players, factory-function
 const Player = (player) => {
 
 }
 
 
-function createBoard() {
 
-    let board = document.querySelector('#board-container');
-    let boardSquare = document.createElement('div');
-
-    for (let i = 0; i < 9; i++) {
-        let boardSquare = document.createElement('div');
-        boardSquare.style.border = '1px solid black'
-        board.style.gridTemplateColumns = `repeat(3, 1fr)`;
-        board.style.gridTemplateRows = `repeat(3, 1fr)`;
-
-        boardSquare.addEventListener('mouseenter', () => {
-            boardSquare.style.background = 'lightgrey'
-        });
-        boardSquare.addEventListener('mouseleave', () => {
-            boardSquare.style.background = 'white';
-        })
-
-        board.appendChild(boardSquare)
-    };
-}
-createBoard()
 
 
