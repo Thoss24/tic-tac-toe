8a41805d69b994fdbc0ff14b2145f60b5ce58c7b
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-04 15:47:31 +0100

    click event selects square and logs player token choice

--- script.js
+++ script.js
@@ -9,19 +9,52 @@ function Players(choice) {
 };
 
 
-let gameBoard = (function() {
+const gameBoard = (function() {
     'use strict'
 
-    let ticTacToeBoard = [];
+    let ticTacToeBoard = ["X1", "X2", "X3", "O4", "O5", "O6", "X7", "X8", "X9"];
+
+    const setBoardChoice = (boardIndex, choice) => {
+        ticTacToeBoard[boardIndex] = choice
+    }
 
     return {
-            ticTacToeBoard: ticTacToeBoard,
-    };
+        setBoardChoice
+    }
+})();
 
+const displayController = (() => {
+
+    let boardSquare = document.querySelectorAll('.board-square');
+    boardSquare.forEach((square) => square.addEventListener('click', (e) => {
+        gameFlow.gameTurn(e.target)
+    }));
+
+    return {
+        boardSquare: boardSquare,
+    }
 })();
 
 
+const gameFlow = (() => {
+    const playerX = Players('X');
+    const playerO = Players('O');
 
+    const gameTurn = (boardPosition) => {
+        gameBoard.setBoardChoice(boardPosition, getPlayerChoice());
+        console.log(getPlayerChoice(), boardPosition)
+    }
+    
+    
+    const getPlayerChoice = () => {
+        return playerX.getChoice()
+    }
+
+    return {
+        gameTurn
+    }
+
+})();
 
 
 
