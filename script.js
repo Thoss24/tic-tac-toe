2faf26235868dc557cd0ad3ba3d07379c3a79354
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-07 13:38:23 +0100

    win condition array added

--- script.js
+++ script.js
@@ -32,7 +32,8 @@ const gameBoard = (function() {
     return {
         setBoardChoice,
         getBoardChoice,
-        resetBoardChoice
+        resetBoardChoice,
+        ticTacToeBoard
     }
 })();
 
@@ -68,7 +69,6 @@ const displayController = (() => {
     
     ();
 
-
 const gameFlow = (() => {
     const playerX = Players('X');
     const playerO = Players('O');
@@ -97,12 +97,29 @@ const gameFlow = (() => {
         userTurn = 1
     }
 
+    const gameOutcome = (boardIndex) => {
+    
+    let winCondition = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+        [1, 4, 7],
+        [2, 5, 8]
+    ]
+    
+
+    }
+    gameOutcome()
 
     return {
         gameTurn, 
         getPlayerChoice, 
         announceRound,
         resetTurn, 
+        gameOutcome,
         roundAnnounce
     }
 
